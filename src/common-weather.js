df80a72ced4183f6c1da669ba842236170e922d8
Alixes <ixes@mail.ru>
2024-09-10 12:20:38 +0300

    fix function getWeather

--- src/common-weather.js
+++ src/common-weather.js
@@ -4,13 +4,23 @@ export async function getWeather() {
   const data = await fetch(
     "https://api.nasa.gov/insight_weather/?api_key=2zR71FbfLVWLCsK2fFTMVDJb7K74EGjO0YeGtPoG&feedtype=json&ver=1.0"
   )
-    .then((res) => {
-      return res.json();
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject({
+          status: response.status,
+          statusText: response.statusText,
+        });
+      }
     })
     .then((data) => {
       console.log(data);
     })
     .catch((err) => {
+      const errContainer = document.createElement("div");
+      errContainer.textContent = `Error: ${err.status} ${err.statusText}`;
+      main.appendChild(errContainer);
       console.log("Ошибка. Запрос не выполнен " + err.message);
     });
 
