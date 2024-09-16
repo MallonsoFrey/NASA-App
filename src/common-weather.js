647d8c3db73eea6924a54dd456e133770dfd86df
Alixes <ixes@mail.ru>
2024-09-16 22:22:09 +0300

    rewrote function renderWeather

--- src/common-weather.js
+++ src/common-weather.js
@@ -1,4 +1,5 @@
-import { mainContent, weather } from "./vars-weather";
+import { mainContent } from "./vars-weather";
+import { main } from "../apod";
 
 export async function getWeather() {
   await fetch(`http://localhost:3001/sol_keys`)
@@ -24,52 +25,47 @@ export async function getWeather() {
     });
 }
 
-function renderWeather(jso) {
+function renderWeather(data) {
   //mainContent.remove();
-  mainContent.innerHTML = `<h2>Current Weather on Mars</h2>`;
-  const sol_keys = jso;
-  for (let i = 0; i < sol_keys.length; i++) {
+  mainContent.innerHTML = `<h2 class="main-title">Current Weather on Mars</h2>
+            <p>NASA’s InSight Mars lander takes continuous weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth plain near Mars’ equator.</p>
+            <p>I'm using InSight Mars Weather Service API to gather this information. View the API
+                <a href="https://api.nasa.gov/assets/insight/InSight%20Weather%20API%20Documentation.pdf" target="_blank">documentation</a> to see how I gathered the information!
+            </p>
+            <h2 class="main-title">Previous 7 Days</h2>
+            `;
+  mainContent.className = "apod_content";
+  main.style.cssText += `
+    top: 0;
+    flex-grow: 1;
+    `;
+
+  const sol_keys = data;
+  const countObj = sol_keys[0];
+  const keys = Object.keys(countObj);
+  let firstKey = keys[0];
+
+  sol_keys.forEach((key) => {
     const sol = document.createElement("div");
-
     sol.classList.add("sol");
+
     const solTitle = document.createElement("h3");
-    solTitle.textContent = sol_keys[i];
-
+    solTitle.textContent = `Day ${key[firstKey].name}`;
     sol.appendChild(solTitle);
-    mainContent.append(sol);
 
-    /*
     const solDate = document.createElement("p");
-    solDate.textContent = `${LOCAL_DATE_TIME[i]} / ${LOCAL_SOL[i]}`;
+    solDate.textContent = `Sunrise: ${key[firstKey].First_UTC}; Sunset: ${key[firstKey].Last_UTC}`;
     sol.appendChild(solDate);
 
     const solTemp = document.createElement("p");
-    solTemp.textContent = `${MIN_TEMPERATURE[i]} / ${MAX_TEMPERATURE[i]}`;
+    solTemp.textContent = `Temperature Low: ${key[firstKey].AT.mn} / High: ${key[firstKey].AT.mx}`;
     sol.appendChild(solTemp);
 
-    const solWind = document.createElement("p");
-    solWind.textContent = `${WIND_SPEED[i]} / ${WIND_DIRECTION[i]}`;
-    sol.appendChild(solWind);
+    const solSeason = document.createElement("p");
+    solSeason.textContent = `Season: ${key[firstKey].Season}`;
+    sol.appendChild(solSeason);
 
-    const solAttitude = document.createElement("p");
-    solAttitude.textContent = `${ATTITUDE[i]}`;
-    sol.appendChild(solAttitude);
-
-    const solHemisphere = document.createElement("p");
-    solHemisphere.textContent = `${HEMISPHERE[i]}`;
-    sol.appendChild(solHemisphere);
-
-    const solDescription = document.createElement("p");
-    solDescription.textContent = `${DESCRIPTION[i]}`;
-    sol.appendChild(solDescription);
-
-    const solHorizon = document.createElement("p");
-    solHorizon.textContent = `${HORIZON[i]}`;
-    sol.appendChild(solHorizon);
-
-    const solAtmosphere = document.createElement("p");
-    solAtmosphere.textContent = `${ATMOSPHERE[i]}`;
-    sol.appendChild(solAtmosphere);
-*/
-  }
+    mainContent.append(sol);
+    firstKey = +firstKey + 1;
+  });
 }
