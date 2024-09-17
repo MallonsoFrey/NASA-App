e6510a846d8bff631a53d75735fd64032f8d8907
MalonsoFrey <miss.dunaewa201141@gmail.com>
2024-09-17 21:40:43 +0400

    mars rover completed, minor style changes in main

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { weather } from "./src/vars-weather";
 import { getWeather } from "./src/common-weather";
 import { apodLink, getData } from "./src/apod";
-import { marsButton, marsCam} from "./src/mars-rovers";
+import { marsButton, mainInfo} from "./src/mars-rovers";
+const spaceGazeButton = document.querySelector('.space-gaze')
+const mainContent = document.querySelector('.main__content')
 
 apodLink.addEventListener("mouseover", function () {
   this.textContent = "Astronomy Picture of the Day";
@@ -13,4 +15,16 @@ apodLink.addEventListener("mouseout", function () {
 
 apodLink.addEventListener("click", getData);
 weather.addEventListener("click", getWeather);
-marsButton.addEventListener('click', marsCam)
+marsButton.addEventListener('click', mainInfo)
+spaceGazeButton.addEventListener('click', spaceGazeRender)
+
+function spaceGazeRender() {
+  mainContent.innerHTML =`
+    <div class="main__content">
+      <h1 class="main__content-heading">SPACE GAZE</h1>
+      <p class="main__content-paragraph">
+        some info about how great your website is
+      </p>
+    </div>
+    `
+}
