8acfd31cb79dffead8a2c0ed57b700764b4f2b02
MalonsoFrey <miss.dunaewa201141@gmail.com>
2024-09-17 21:40:43 +0400

    mars rover completed, minor style changes in main

--- src/mars-rovers.js
+++ src/mars-rovers.js
@@ -1,31 +1,8 @@
 export const marsButton = document.querySelector('.mars')
 const output = document.querySelector('.main__content')
 
-export function marsCam() {
-    mainInfo()
 
-    const chosenSol = document.querySelector('#sol')
-    const chosenCam = document.querySelector('#cam')
-    const roverButton = document.querySelector('.roverButton')
-
-    roverButton.addEventListener('click', () => {
-        const chosenSolValue = chosenSol.value
-        const chosenCamValue = chosenCam.value
-
-        if (chosenCamValue === '' || chosenSolValue > 4300 || chosenSolValue <= 0) {
-            output.innerHTML = `<p>Please enter valid Sol or choose a camera before proceeding.</p>
-            <button class="return">Go back</button>`
-            
-            const goBackButton = document.querySelector('.return');
-            goBackButton.addEventListener('click', mainInfo);
-            return
-        }
-
-        marsRender(chosenSolValue, chosenCamValue)
-    })
-}
-
-function mainInfo() {
+export function mainInfo() {
     output.innerHTML = `
     <div class="mars-container">
         <h3>Here you can find out everything about the rover Curiosity</h3>
@@ -40,12 +17,33 @@ function mainInfo() {
             <option value="mardi">Descent Imager</option>
             <option value="navcam">Navigation Camera</option>
         </select>
-        <button class="roverButton return">Show me!</button>
+        <button class="roverButton buttonStyle">Show me!</button>
     </div>
     `
+
+    const chosenSol = document.querySelector("#sol")
+    const chosenCam = document.querySelector("#cam")
+    const roverButton = document.querySelector(".roverButton") 
+    roverButton.addEventListener("click", () => 
+        marsRender(chosenSol.value, chosenCam.value) 
+    ) 
+
 }
 
 async function marsRender(num, cam) {
+    if (cam === "" || num > 4300 ||  num <= 0) { 
+        output.innerHTML = `
+        <div class="mars-container">
+            <p>Please enter valid Sol or choose a camera before proceeding.</p> 
+            <button class="returnButton buttonStyle">Go back</button>
+        </div>
+        ` 
+
+        const goBackButton = document.querySelector(".returnButton") 
+        goBackButton.addEventListener("click", mainInfo); 
+        return 
+    }
+
     output.innerHTML = '<img src="./src/images/loading.gif" alt="rocket loading gif">'
     try {
         const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${num}&camera=${cam}&api_key=ujhHC5aAHogxEryayLSB4H3LRoKlpAkpqq48JZ8g`)
@@ -59,9 +57,11 @@ async function marsRender(num, cam) {
             output.innerHTML = `
                 <div class="mars-container">
                     <p>No photos available for this Sol or camera. Please try another selection.</p>
-                    <button class="return">Go back</button>
+                    <button class="returnButton buttonStyle">Go back</button>
                 </div>
             `
+            const goBackButton = document.querySelector('.returnButton')
+            goBackButton.addEventListener('click', mainInfo)
             return
         }
 
@@ -71,20 +71,19 @@ async function marsRender(num, cam) {
                 <div style="width: 50%"><img style="max-width: 100%; height: auto;" src="${data.photos[0].img_src}" alt="Mars Rover Photo"></div>
                 <p>Rover: ${data.photos[0].rover.name}</p>
                 <p>Date: ${data.photos[0].earth_date}</p>
-                <button class="return">Go back</button>
+                <button class="returnButton buttonStyle">Go back</button>
             </div>
         `
-        const goBackButton = document.querySelector('.return');
+        const goBackButton = document.querySelector('.returnButton');
         goBackButton.addEventListener('click', mainInfo);
     } catch (error) {
         output.innerHTML = `
         <div class="mars-container">
             <p>Error fetching data: ${error.message}</p><>
-            <button class="return">Go back</button>
+            <button class="returnButton buttonStyle">Go back</button>
         </div>
         `
-
-        const goBackButton = document.querySelector('.return');
+        const goBackButton = document.querySelector('.returnButton');
         goBackButton.addEventListener('click', mainInfo);
     } finally {
         console.log('Request was made')
